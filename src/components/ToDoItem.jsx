2da68f011d02f58c36081ff19d0280f1412071dc
mithul nallanagula <mithulnallanaula@gmail.com>
2025-01-28 20:26:58 +0530

    changing icons

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -9,8 +9,8 @@ const Lits = (prop) => {
             <img src={`/src/assets/images/${check ? 'check.png' : 'uncheck.png'}`} onClick={() => setcheck(!check)} className='w-6 h-6 ml-1 cursor-pointer'></img>
             <p className={`font-bold text-2xl ml-1 mb-1 cursor-pointer ${check ? "line-through text-gray-500" : ""}`} onClick={() => setcheck(!check)} >{prop.editing ? "Updating...." : prop.task}</p>
             <div className="flex ml-auto space-x-2 mr-2  ">
-              
-              <img src='/src/assets/images/edit.png' className='w-6 h-6 ml-1 cursor-pointer' onClick={() => check ? null : prop.edit(prop.index)}></img>
+            <i className="ri-edit-box-fill" onClick={() => check ? null : prop.edit(prop.index)} ></i>
+             
             <img src='/src/assets/images/delete.png' className='w-6 h-6 ml-1 cursor-pointer' onClick={() => prop.deletetask(prop.index) }></img>
 </div>
         </div>
